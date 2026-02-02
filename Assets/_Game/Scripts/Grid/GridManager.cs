25e2dd94c4acd5a4dd4550a8372ace6accb24663
Harry <hoangnguyen72.dev@gmail.com>
2026-02-02 17:27:07 +0700

    first init

---
+++ Assets/_Game/Scripts/Grid/GridManager.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace _Game.Scripts.Grid
+{
+    public class GridManager : MonoBehaviour
+    {
+        #region Variables
+        public static GridManager Instance;
+        
+        [Header("Settings")] 
+        public int rows = 9;
+        public int cols = 16;
+        public Node nodePrefab;
+        public float cellSize = 1.1f;
+
+        [Header("Debug")] 
+        public Node[,] grid;
+        
+        private Node selectedNode;
+        #endregion
+        
+        #region Unity Callbacks
+        private void Awake()
+        {
+            Instance = this;
+        }
+
+        private void Start()
+        {
+            GenerateGrid();
+        }
+        #endregion
+        
+        #region Public Methods
+        private void GenerateGrid()
+        {
+            // 1. Setup data
+            List<int> valueList = new List<int>();
+            int totalCells = cols * rows;
+            int pairsCount = 4;
+            int uniqueSprites = totalCells / pairsCount;
+
+            for (int i = 1; i <= uniqueSprites; i++)
+            {
+                for (int j = 0; j < pairsCount; j++)
+                {
+                    valueList.Add(i);
+                }
+            }
+            
+            // 2. Shuffle list
+            Shuffle(valueList);
+
+            // 3. Visual
+            grid = new Node[cols, rows];
+
+            Vector3 startPos = new Vector3(
+                -(cols * cellSize) / 2.0f + cellSize / 2,
+                -(rows * cellSize) / 2.0f + cellSize / 2,
+                0
+            );
+
+            int index = 0;
+
+            for (int x = 0; x < cols; x++)
+            {
+                for (int y = 0; y < rows; y++)
+                {
+                    Node newNode = Instantiate(nodePrefab, transform);
+
+                    Vector3 pos = new Vector3(x * cellSize, y * cellSize, 0) + startPos;
+                    newNode.transform.position = pos;
+
+                    int value = valueList[index];
+                    newNode.SetData(x, y, value);
+                    
+                    grid[x, y] = newNode;
+                
+                    index++;
+                }
+            }
+        }
+        #endregion
+
+        #region Private Methods
+        // Fisher-Yates Algorithm
+        private void Shuffle(List<int> list)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                int temp = list[i];
+                int randomIndex = UnityEngine.Random.Range(i, list.Count);
+                list[i] = list[randomIndex];
+                list[randomIndex] = temp;
+            }
+        }
+        #endregion
+    }
+}
---
+++ Assets/_Game/Scripts/Grid/Node.cs
@@ -0,0 +1,51 @@
+using TMPro;
+using UnityEngine;
+
+namespace _Game.Scripts.Grid
+{
+    public class Node : MonoBehaviour
+    {
+        #region Variables
+        [Header("Data")] 
+        public int id;
+        public int x, y;
+        public bool isMatched = false;
+        
+        [Header("Visual")]
+        public TextMeshPro textMesh; // Tạm thay thế Image/Sprite
+        public SpriteRenderer bgRenderer; // Đổi màu nền khi chọn
+        #endregion
+        
+        #region Methods
+        public void SetData(int dx, int dy, int did)
+        {
+            this.x = dx;
+            this.y = dy;
+            this.id = did;
+            this.isMatched = false;
+
+            if (textMesh != null)
+            {
+                textMesh.text = id.ToString();
+            }
+
+            gameObject.name = $"Node_{x}_{y}";
+        }
+
+        private void OnMouseDown()
+        {
+            if (isMatched) return;
+            
+            //GridManager.Instance.OnNodeClicked(this);
+        }
+
+        public void SetSelected(bool isSelected)
+        {
+            if (bgRenderer != null)
+            {
+                bgRenderer.color = isSelected ? Color.green : Color.red;
+            }
+        }
+        #endregion
+    }
+}
