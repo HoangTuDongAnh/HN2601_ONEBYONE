5cac2489b6370d0629aca4fd6af2bc1775baebe8
Harry <hoangnguyen72.dev@gmail.com>
2026-02-03 10:46:16 +0700

    feat: add SO

---
+++ Assets/_Game/Scripts/ScriptableObjects/LevelData.cs
@@ -0,0 +1,20 @@
+﻿using _Game.Scripts.Utils;
+using UnityEngine;
+
+namespace _Game.Scripts.ScriptableObjects
+{
+    [CreateAssetMenu(fileName = "Level_01", menuName = "Game Data/Level Data")]
+    public class LevelData : ScriptableObject
+    {
+        [Header("General Info")]
+        public int levelIndex;
+        public float timeLimit = 300f;
+
+        [Header("Grid Settings")]
+        public int rows = 9;
+        public int columns = 16;
+
+        [Header("Difficulty")]
+        public GravityDirection gravityDirection;
+    }
+}
---
+++ Assets/_Game/Scripts/ScriptableObjects/ThemeData.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace _Game.Scripts.ScriptableObjects
+{
+    [CreateAssetMenu(fileName = "NewTheme", menuName = "Game Data/Theme Data")]
+    public class ThemeData : ScriptableObject
+    {
+        [Header("Collection")]
+        public List<Sprite> icons; 
+        
+        [Header("Background")]
+        public Sprite backgroundSprite; 
+        
+        public Sprite GetSpriteById(int id)
+        {
+            // ID trong game logic của bạn đang là 1 -> 36
+            // List index là 0 -> 35. Nên cần trừ 1
+            int index = id - 1; 
+            
+            if (index >= 0 && index < icons.Count)
+            {
+                return icons[index];
+            }
+            return null; 
+        }
+    }
+}
