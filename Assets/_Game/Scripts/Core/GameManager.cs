9215ca30cf72efdfb08030df39d03a51d28a58f2
Harry <hoangnguyen72.dev@gmail.com>
2026-02-03 10:47:47 +0700

    feat: add game manager

---
+++ Assets/_Game/Scripts/Core/GameManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using _Game.Scripts.Grid;
+using _Game.Scripts.ScriptableObjects;
+
+namespace _Game.Scripts.Core
+{
+    public class GameManager : MonoBehaviour
+    {
+        public static GameManager Instance;
+
+        [Header("Data")]
+        public ThemeData currentTheme;       // Kéo file Theme vào đây
+        public List<LevelData> levels;       // Kéo các file Level 1, 2, 3... vào đây
+
+        [Header("State")]
+        public int currentLevelIndex = 0;
+
+        private void Awake()
+        {
+            Instance = this;
+        }
+
+        private void Start()
+        {
+            StartLevel(currentLevelIndex);
+        }
+
+        public void StartLevel(int index)
+        {
+            if (index >= levels.Count)
+            {
+                Debug.Log("Đã hết màn chơi! You Win!");
+                return;
+            }
+
+            currentLevelIndex = index;
+            LevelData data = levels[index];
+
+            Debug.Log($"Đang tải Level {data.levelIndex} - Gravity: {data.gravityDirection}");
+
+            GridManager.Instance.InitializeLevel(data, currentTheme);
+        }
+
+        public void NextLevel()
+        {
+            StartLevel(currentLevelIndex + 1);
+        }
+    }
+}
